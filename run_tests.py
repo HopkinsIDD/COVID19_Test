4e4a900ebf6661ceef989e682b489ec2a06ad98c
kkintaro <katkintaro@gmail.com>
2020-04-04 20:03:38 -0400

    Updated data and config to actually run everythin

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,19 @@
 import pytest
 import os
+from pathlib import Path
 import subprocess
 import multiprocessing
 
-def a_test(test_dir):
+
+# assert helpers
+def assert_file(file):
+    assert Path(file).is_file(), f"{file} is not a file"
+
+def assert_dir(file):
+    assert Path(file).is_dir(), f"{file} is not a dir"
+
+# main test runner helper
+def _success(test_dir):
     os.chdir(test_dir)
     subprocess.run(["make", "clean"])
 
@@ -13,20 +23,34 @@ def a_test(test_dir):
             "-p", "../COVIDScenarioPipeline",
             "-n", str(multiprocessing.cpu_count())]
     complete = subprocess.run(cmd)
-    assert complete.returncode == 0
-    assert os.path.exists("Makefile")
+    assert complete.returncode == 0, f"make_makefile.R failed with code {complete.returncode}"
+
+    assert_file("Makefile")
 
     # Run the Makefile
     cmd = ["make"]
     complete = subprocess.run(cmd)
-    assert complete.returncode == 0
+    assert complete.returncode == 0, f"make failed with code {complete.returncode}"
 
+    assert_dir("model_output")
+    assert_dir("hospitalization")
+
+# teardown
 def teardown_function(self):
     subprocess.run(["make", "clean"])
     os.chdir("..")
 
+
+# Test definitions
+
 def test_1():
-    a_test("test1")
+    _success("test1")
 
 def test_importation():
-    a_test("test_importation")
+    _success("test_importation")
+
+    assert_file("data/filter.txt")
+    assert_dir("data/HI/shp")
+    assert_dir("importation")
+
+
